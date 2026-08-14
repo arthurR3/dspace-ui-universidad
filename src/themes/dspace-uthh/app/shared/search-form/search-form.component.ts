e20ddb62154e0c9331188d59afb0365533756916
arthurR3 <hedzjose014@gmail.com>
2026-08-14 09:55:08 -0600

    feat(cards): updated button color

--- src/themes/dspace-uthh/app/shared/search-form/search-form.component.ts
+++ src/themes/dspace-uthh/app/shared/search-form/search-form.component.ts
@@ -4,8 +4,8 @@ import { FormsModule } from '@angular/forms';
 import { NgbTooltip } from '@ng-bootstrap/ng-bootstrap';
 import { TranslateModule } from '@ngx-translate/core';
 
-import { SearchFormComponent as BaseComponent } from '../../../../../../app/shared/search-form/search-form.component';
-import { BrowserOnlyPipe } from '../../../../../../app/shared/utils/browser-only.pipe';
+import { SearchFormComponent as BaseComponent } from '../../../../../app/shared/search-form/search-form.component';
+import { BrowserOnlyPipe } from '../../../../../app/shared/utils/browser-only.pipe';
 
 @Component({
   selector: 'ds-themed-search-form',
